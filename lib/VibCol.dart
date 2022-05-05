865670f8f3923a2fe44cfe7178519dd4a126e134
jk <aspirinii@naver.com>
2022-05-05 23:16:55 +0900

    firstTime

---
+++ lib/VibCol.dart
@@ -0,0 +1,109 @@
+import 'package:vibration/vibration.dart';
+
+List<int> part1Pattern1(int sec){
+    List<int> startPattern =  [50,100,50,100, 700];
+    List<int> inPattern =  [for(var i=0; i<sec*2; i+=1) if (i%2 == 0) 100 else 900];
+    List<int> vPattern1 =  startPattern + inPattern;
+    return vPattern1;
+}
+
+List<int> part1Inten1(int sec){
+    List<int> startIntensities =  [0, 255,0,255, 0];
+    List<int> inIntensities =  [for(var i=0; i<sec*2; i+=1) if (i%2 == 0) 100 else 0];
+    List<int> vInten1 =  startIntensities + inIntensities;
+    return vInten1;
+}
+
+
+List<int> part2Pattern2(int sec){
+  int fsmo = sec-1;
+  List<int> vPattern2 =  [];
+  if (fsmo < 0) {
+    vPattern2 =  [];
+  }
+  else {                  
+    vPattern2 =  [fsmo*1000,1000];
+  }
+  return vPattern2;
+
+}
+
+List<int> part2Inten2(int sec){
+  int fsmo = sec-1;
+  List<int> vIntensities2 = [];
+  if (fsmo < 0) {
+    vIntensities2 = [];
+  }
+  else {                  
+    vIntensities2 = [100,0];
+  }
+  return vIntensities2;
+}
+
+List<int> part1Pattern(int sec){
+  var index = 5;
+  List<int> vPattern3= [for(var i=0; i<sec*index; i++) (1000/index).toInt()];
+  return vPattern3;
+
+}
+
+List<int> part1Inten(int sec){
+  var index = 5;
+  var maxInten = 100;
+  var step = maxInten/(sec*index); 
+  List<int> vIntensities3 = [for(double i=0; i<sec*index; i++) (i*step).toInt()];
+  return vIntensities3;
+}
+
+
+List<int> part2Pattern(int sec){
+    List<int> vPattern1 =  [for(var i=0; i<sec*2; i+=1) if (i%2 == 0) 100 else 900];
+    return vPattern1;
+}
+
+List<int> part2Inten(int sec){
+    List<int> vInten1 =  [for(var i=0; i<sec*2; i+=1) if (i%2 == 0) 100 else 0];
+    return vInten1;
+}
+
+List<int> part3Pattern(int sec){
+  var index = 5;
+  List<int> vPattern3= [for(var i=0; i<sec*index; i++) (1000/index).toInt()];
+  return vPattern3;
+
+}
+
+List<int> part3Inten(int sec){
+  var index = 5;
+  var maxInten = 100;
+  var step = maxInten/(sec*index); 
+  List<int> vIntensities = [for(double i=0; i<sec*index; i++) (i*step).toInt()];
+  List<int> vIntensities3 = new List.from(vIntensities.reversed);
+  return vIntensities3;
+}
+
+void HapticVib(int cycle, int inhaleSec, int fullSec, int exhaleSec){
+
+  List<int> vPattern1 =  part1Pattern(inhaleSec);
+  List<int> vIntensities1 = part1Inten(inhaleSec);
+
+  List<int> vPattern2 =  part2Pattern(fullSec);
+  List<int> vIntensities2 = part2Inten(fullSec);
+
+  List<int> vPattern3 =  part3Pattern(exhaleSec);
+  List<int> vIntensities3 = part3Inten(exhaleSec);
+
+  var sumP = vPattern1 + vPattern2 + vPattern3;
+  var sumI = vIntensities1 + vIntensities2 + vIntensities3;
+  List<int>  combinePattern = [];
+  List<int>  combineIntensities = [];
+  for (int i=0; i<cycle; i++){
+    combinePattern.addAll(sumP);
+    combineIntensities.addAll(sumI);
+  }
+
+  Vibration.vibrate(
+    pattern: combinePattern,
+    intensities: combineIntensities,
+  );
+}
